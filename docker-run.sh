9584c9b683f5156c8056c1bf9b8da2896e5485f6
Matt Hayward <matt@hayward.solutions>
2024-12-19 00:27:26 +0000

    Debugging

--- docker-run.sh
+++ docker-run.sh
@@ -3,11 +3,13 @@
 # Sharing for FTSConfig.yaml
 echo "Starting FreeTAKServer"
 
-cat /opt/fts/FTSConfig.yaml
+cat /opt/fts/FTSConfig.yaml || echo "FTSConfig.yaml not found."
 
 if [[ ! -f "/opt/fts/FTSConfig.yaml" ]]
   then
+    echo "Generating FTSConfig.yaml"
     python -c "from FreeTAKServer.core.configuration.configuration_wizard import autogenerate_config; autogenerate_config()"
 fi
 
+echo "Starting FreeTAKServer"
 python -m FreeTAKServer.controllers.services.FTS
